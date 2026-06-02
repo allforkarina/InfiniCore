f0b2a628eb6b77f5d27127f7f72812fdf853d0a8
allforkarina <liu050804@gmail.com>
2026-06-03 02:50:41 +0800

    Revert "Default roll tests to ntops devices"
    This reverts commit cee52a1be44c166d008289fabfd5692060289b44.

--- python/infinicore/nn/functional/roll.py
+++ python/infinicore/nn/functional/roll.py
@@ -6,9 +6,8 @@ def roll(input: Tensor, shifts: int, dims: int = 0, *, out=None) -> Tensor:
     if infinicore.use_ntops and input.device.type in ("cuda", "musa") and out is None:
         return infinicore.ntops.torch.roll(input, shifts, dims)
 
-    if out is not None:
-        raise NotImplementedError("roll with out= is not implemented")
+    if out is None:
+        return Tensor(_infinicore.roll(input._underlying, shifts, dims))
 
-    raise NotImplementedError(
-        "roll is currently implemented through ntops on cuda/musa devices only"
-    )
+    _infinicore.roll_(out._underlying, input._underlying, shifts, dims)
+    return out
